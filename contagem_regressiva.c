ac20c01e7daa48fdbf5c67e8449721ed11da3238
aryelly-dev <aryellym@icloud.com>
2025-07-29 09:36:29 -0300

    Adiciona arquivo contagem_regressiva.c
    Implementa contagem regressiva de 100 a 1.
    
    O programa inicia a contagem a partir de 100 e decrementa até 1, exibindo os números no terminal. 
    A repetição é controlada com a estrutura de repetição While, que verifica a condição até que o valor atinja 1.

---
+++ contagem_regressiva.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int cont = 100;
+
+    printf("Contagem regressiva de 100 a 1:\n");
+
+    while (cont > 0) {
+        printf("%d\n", cont);
+        cont--;
+    }
+
+return 0;
+}
