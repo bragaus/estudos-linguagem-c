6a4862d787120f8c245e33f77df61fe5318c0122
aryelly-dev <aryellym@icloud.com>
2025-07-29 09:27:51 -0300

    Adiciona arquivo contagem_progressiva.c
    Implementa contagem progressiva de 1 a 100 com a estrutura de repetição While.

---
+++ contagem_progressiva.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int cont = 1;
+
+    printf("Contagem progressiva de 1 a 100:\n");
+    while (cont <= 100) {
+        printf("%d\n", cont);
+        cont++;
+    }
+
+return 0;
+}
