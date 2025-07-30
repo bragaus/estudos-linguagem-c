b5bc4c115d7ff037f97acad26b5457347c944c51
Experimentos em Série <sprtj@protonmail.com>
2025-07-30 00:01:23 -0300

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,28 @@
-#include <stdio.h>
 
-int main()
-{
-    printf("Hello World!");
 
-    return 0;
+long write_syscall(long fd, const void *buf, unsigned long count) {
+
+    return __builtin_syscall(1, fd, buf, count);
+}
+
+
+
+void exit_syscall(int status) { __builtin_syscall(60, status);
+}
+
+
+
+
+
+
+void _start() {
+    char msg[] = "Hello World!";
+
+
+
+
+    
+
+    write_syscall(1, msg, sizeof(msg) - 1);
+    exit_syscall(0); 
 }
